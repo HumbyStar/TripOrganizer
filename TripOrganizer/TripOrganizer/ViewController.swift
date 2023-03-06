2aad261361d39067966a2af6ad44cb1b01b3016a
Humberto <betogrt300@gmail.com>
2023-03-06 15:03:13 -0300

    Added project in repository

---
+++ TripOrganizer/TripOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripOrganizer
+//
+//  Created by Humberto Rodrigues on 06/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+    }
+
+
+}
+
