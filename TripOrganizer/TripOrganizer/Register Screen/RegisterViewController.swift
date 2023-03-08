9eb6d47e668f1a1d292b5649d5107eec06750b46
Felipe Augusto <augusto_felipe@icloud.com>
2023-03-08 12:04:22 -0300

    adicionando textfield e botões.

---
+++ TripOrganizer/TripOrganizer/Register Screen/RegisterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterViewController.swift
+//  TripOrganizer
+//
+//  Created by Felipe Augusto Correia on 08/03/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
