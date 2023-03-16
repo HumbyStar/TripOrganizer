3b27c64efa49940f55b41f3b7b1f9164a8177172
Eduardo Escaleira <duescaleira@gmail.com>
2023-03-16 18:29:32 -0300

    no message

--- TripOrganizer/TripOrganizer/TabBar/Flight/FlightVC.swift
+++ TripOrganizer/TripOrganizer/TabBar/Flight/FlightVC.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class FlightVC: UIViewController {
+    
+    
+    @IBOutlet weak var flightOriginView: UIView!
+    @IBOutlet weak var flightOriginTextField: UITextField!
 
+    @IBOutlet weak var calendarOnGoingView: UIView!
+    @IBOutlet weak var calendarOnGoingTextField: UITextField!
+    
+    @IBOutlet weak var calendarOutGoingView: UIView!
+    @IBOutlet weak var calendarOutGoingTextField: UITextField!
+   
+    @IBOutlet weak var flightDestinationView: UIView!
+    @IBOutlet weak var flightDestinationTextField: UITextField!
+   
+    
+    @IBOutlet weak var passengersView: UIView!
+    @IBOutlet weak var passengersTextField: UITextField!
+    
+    
+    @IBOutlet weak var ticketSearchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configElements()
 
-        // Do any additional setup after loading the view.
     }
     
+    private func configElements() {
+        
+        flightOriginTextField.borderStyle = .none
+        flightDestinationTextField.borderStyle = .none
+        calendarOnGoingTextField.borderStyle = .none
+        calendarOutGoingTextField.borderStyle = .none
+        passengersTextField.borderStyle = .none
 
-    /*
-    // MARK: - Navigation
+        
+        flightOriginView.layer.cornerRadius = 5
+        flightOriginView.clipsToBounds = true
+        flightDestinationView.layer.cornerRadius = 5
+        flightDestinationView.clipsToBounds = true
+        calendarOnGoingView.layer.cornerRadius = 5
+        calendarOnGoingView.clipsToBounds = true
+        calendarOutGoingView.layer.cornerRadius = 5
+        calendarOutGoingView.clipsToBounds = true
+        passengersView.layer.cornerRadius = 5
+        passengersView.clipsToBounds = true
+        ticketSearchButton.layer.cornerRadius = 5
+        ticketSearchButton.clipsToBounds = true
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
