276168b87ffe44ea651fd8f1a68737c3e3f4743f
Felipe Augusto <augusto_felipe@icloud.com>
2023-03-16 13:10:31 -0300

    attraction screen finished

--- TripOrganizer/TripOrganizer/TabBar/Attraction/AttractionVC.swift
+++ TripOrganizer/TripOrganizer/TabBar/Attraction/AttractionVC.swift
@@ -6,24 +6,70 @@
 //
 
 import UIKit
+import MapKit
 
 class AttractionVC: UIViewController {
+    
+    let locationImagelist: [String] = ["1","2","3","4","5"]
 
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var map: MKMapView!
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var detailView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configCollectionView()
+        configViewStyle()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configViewStyle() {
+        detailView.layer.borderWidth = 1.5
+        detailView.layer.borderColor = UIColor.white.cgColor
+        detailView.clipsToBounds = true
+        detailView.layer.cornerRadius = 10
+        
+        map.layer.borderWidth = 1.5
+        map.layer.borderColor = UIColor.white.cgColor
+        map.clipsToBounds = true
+        map.layer.cornerRadius = 10
+        
+        collectionView.backgroundColor = .white
+        
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+            textField.backgroundColor = UIColor.white
+        }
     }
-    */
+    
+    func configCollectionView() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            
+            layout.scrollDirection = .horizontal
+            layout.estimatedItemSize = .zero
+        }
+        
+        collectionView.register(AttractionCell.nib(), forCellWithReuseIdentifier: AttractionCell.identifier)
+    }
+}
 
+extension AttractionVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return locationImagelist.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: AttractionCell? = collectionView.dequeueReusableCell(withReuseIdentifier: AttractionCell.identifier, for: indexPath) as? AttractionCell
+        cell?.setupCell(imageName: locationImagelist[indexPath.row])
+        return cell ?? UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120, height: 120)
+    }
+    
 }
---
+++ TripOrganizer/TripOrganizer/TabBar/Attraction/Cell/AttractionCell.swift
@@ -0,0 +1,30 @@
+//
+//  AttractionCell.swift
+//  TripOrganizer
+//
+//  Created by Felipe Augusto Correia on 16/03/23.
+//
+
+import UIKit
+
+class AttractionCell: UICollectionViewCell {
+    
+    @IBOutlet var attractionImageView: UIImageView!
+    
+    static let identifier: String = "AttractionCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        attractionImageView.contentMode = .scaleAspectFill
+    }
+    
+    func setupCell(imageName: String) {
+        self.attractionImageView.image = UIImage(named: imageName)
+    }
+
+}
