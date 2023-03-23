fabde1a3fc431a22f88fbaa0718598dd4e7a3681
Gabriel Mors <gabrielmors210@gmail.com>
2023-03-23 16:32:05 -0400

    setei as constraints da view

--- TripOrganizer/TripOrganizer/TabBar/Hotel/HotelVC.swift
+++ TripOrganizer/TripOrganizer/TabBar/Hotel/HotelVC.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class HotelVC: UIViewController {
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+            navigationController?.setNavigationBarHidden(true, animated: false)
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +21,5 @@ class HotelVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
