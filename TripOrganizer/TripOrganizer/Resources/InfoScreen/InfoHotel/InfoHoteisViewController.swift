ffb053447f775dcb1d82d0f1cbd83a488c19247c
Gabriel Mors <gabrielmors210@gmail.com>
2023-03-24 15:54:16 -0400

    adicionei 3 elementos na tela

---
+++ TripOrganizer/TripOrganizer/Resources/InfoScreen/InfoHotel/InfoHoteisViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoHoteisViewController.swift
+//  TripOrganizer
+//
+//  Created by Gabriel Mors  on 24/03/23.
+//
+
+import UIKit
+
+class InfoHoteisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
