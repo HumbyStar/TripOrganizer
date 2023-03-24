6e55763ba430bedd2bd5f2327e8d2f41f6ef744f
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-03-24 16:27:07 -0300

    realizei a construção da tela, colocando todos os objetos, setando as constrants, setando a constrant da celula da collection view, adicionando as fotos que necessitamos

---
+++ TripOrganizer/TripOrganizer/TabBar/Restaurant/MenuCell/MenuCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MenuCollectionViewCell.swift
+//  TripOrganizer
+//
+//  Created by Mikael Rodrigues on 24/03/23.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- TripOrganizer/TripOrganizer/TabBar/Restaurant/RestaurantVC.swift
+++ TripOrganizer/TripOrganizer/TabBar/Restaurant/RestaurantVC.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class RestaurantVC: UIViewController {
 
+    @IBOutlet weak var menuCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//configmenuCollectionView()
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   // func configmenuCollectionView(){
+       // menuCollectionView.delegate = self
+        //menuCollectionView.dataSource = self
+        
     }
-    */
+   
 
-}
+//}
+//extension RestaurantVC: UICollectionViewDelegate, UICollectionViewDataSource {
+   // func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    //return 0
+   // }
+    
+   // func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      //  return menuCollectionView.dequeueReusableCell(withReuseIdentifier: <#T##String#>, for:)
+   // }
+    
+    
+    
+    
+    
+    
+//}
+    
+    
+
