f89d6949529bdd23be2795263fe61b74744011d1
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-03-24 19:26:57 -0300

    collectionView funcionando corretamente

--- TripOrganizer/TripOrganizer/TabBar/Restaurant/MenuCell/MenuCollectionViewCell.swift
+++ TripOrganizer/TripOrganizer/TabBar/Restaurant/MenuCell/MenuCollectionViewCell.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class MenuCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var valorLabel: UILabel!
+    @IBOutlet weak var figuraImageView: UIImageView!
+    @IBOutlet weak var fotoImageView: UIImageView!
+    
+    static let identifier: String = "MenuCollectionViewCell"
+             
+    static func nib() -> UINib {
+        return UINib.init(nibName: identifier, bundle: nil)
+        
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+       
+        layer.borderWidth = 1.0
+                layer.borderColor = UIColor.lightGray.cgColor
+                layer.cornerRadius = 8.0
         // Initialization code
     }
-
+    func setupCell(image: String, valor:String){
+        fotoImageView.image = UIImage(named: image)
+        valorLabel.text = valor
+    }
+    
+    
+    
 }
--- TripOrganizer/TripOrganizer/TabBar/Restaurant/RestaurantVC.swift
+++ TripOrganizer/TripOrganizer/TabBar/Restaurant/RestaurantVC.swift
@@ -9,37 +9,55 @@ import UIKit
 
 class RestaurantVC: UIViewController {
 
+    @IBOutlet weak var fulanoLabel: UILabel!
     @IBOutlet weak var menuCollectionView: UICollectionView!
+    
+    var imagens:[String] = ["pratoDeComidaProjeto", "garrafaProjeto", "petiscoProjeto"]
+    var valores:[String] = ["Valor: R$ 30,00", "Valor: R$ 50,00", "Valor: R$ 50,00"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//configmenuCollectionView()
+configmenuCollectionView()
         
-        
-    }
-    
-   // func configmenuCollectionView(){
-       // menuCollectionView.delegate = self
-        //menuCollectionView.dataSource = self
         
     }
    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    func configmenuCollectionView(){
+        menuCollectionView.register(MenuCollectionViewCell.nib(), forCellWithReuseIdentifier: MenuCollectionViewCell.identifier)
+        menuCollectionView.delegate = self
+        menuCollectionView.dataSource = self
+        if let layout = menuCollectionView.collectionViewLayout as? UICollectionViewFlowLayout{
+            layout.scrollDirection = .horizontal
+            layout.estimatedItemSize = .zero
+        }
+            
+    }
+   
 
-//}
-//extension RestaurantVC: UICollectionViewDelegate, UICollectionViewDataSource {
-   // func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    //return 0
-   // }
+}
+extension RestaurantVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imagens.count
+    }
     
-   // func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-      //  return menuCollectionView.dequeueReusableCell(withReuseIdentifier: <#T##String#>, for:)
-   // }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MenuCollectionViewCell.identifier, for: indexPath) as? MenuCollectionViewCell
+        cell?.setupCell(image: imagens[indexPath.row], valor: valores[indexPath.row])
+        return cell ?? UICollectionViewCell()
+    }
+    
+    override func size(forChildContentContainer container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
+        return CGSize(width: 85, height: 111)
+    }
     
     
     
     
-    
-    
-//}
+}
     
     
 
