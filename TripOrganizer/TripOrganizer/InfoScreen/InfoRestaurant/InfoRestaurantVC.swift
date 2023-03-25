96df7969ddc4de2c4c1754d1277657e56dcf0277
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-03-25 01:37:13 -0300

    setei as constraints

--- TripOrganizer/TripOrganizer/InfoScreen/InfoRestaurant/InfoRestaurantVC.swift
+++ TripOrganizer/TripOrganizer/InfoScreen/InfoRestaurant/InfoRestaurantVC.swift
@@ -5,4 +5,27 @@
 //  Created by Mikael Rodrigues on 25/03/23.
 //
 
-import Foundation
+import UIKit
+
+class InfoRestaurantVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
