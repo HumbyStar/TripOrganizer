7813cb0c40640e7b13a147d9a46ac3bf730ef886
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-03-27 16:47:29 -0300

    collectionView criada com sucesso e fazendo algumas melhorias na mesma

--- TripOrganizer/TripOrganizer/InfoScreen/InfoRestaurant/Cell/InfoRestaurantVCCell.swift
+++ TripOrganizer/TripOrganizer/InfoScreen/InfoRestaurant/Cell/InfoRestaurantVCCell.swift
@@ -9,18 +9,25 @@ import UIKit
 
 class InfoRestaurantVCCell: UICollectionViewCell {
     
+    @IBOutlet weak var adicionarButton: UIButton!
+    @IBOutlet weak var cardapioValorLabel: UILabel!
+    @IBOutlet weak var cardapioFotoImageView: UIImageView!
     static let identifier: String = "InfoRestaurantVCCell"
     static func nib()-> UINib{
         return UINib(nibName: identifier, bundle: nil)
-        
+    }
         override func awakeFromNib() {
             super.awakeFromNib()
             // Initialization code
-        }
+            }
         
+    func setupCell(image: String, valor:String){
+            cardapioFotoImageView.image = UIImage(named: image)
+            cardapioValorLabel.text = valor
+            
+        }
     }
-}
-
+    
 
 
 
