1c473f44691e74ac6d0d212d3d49d28f261b04cb
Gabriel Mors <gabrielmors210@gmail.com>
2023-03-28 16:10:19 -0400

    configurei o button da minha celular

--- TripOrganizer/TripOrganizer/InfoScreen/InfoHotel/InfoHoteisViewController.swift
+++ TripOrganizer/TripOrganizer/InfoScreen/InfoHotel/InfoHoteisViewController.swift
@@ -54,6 +54,6 @@ extension InfoHoteisViewController: UICollectionViewDelegate, UICollectionViewDa
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 120, height: 125)
+        return CGSize(width: 150, height: 135)
     }
 }
