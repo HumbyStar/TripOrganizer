71222fbe5eea3c0bc20d7cc6079b3a549d4bbe28
Felipe Augusto <augusto_felipe@icloud.com>
2023-04-09 18:30:03 -0300

    perfil screen created

--- TripOrganizer/TripOrganizer/TabBar/Perfil/PerfilViewController.swift
+++ TripOrganizer/TripOrganizer/TabBar/Perfil/PerfilViewController.swift
@@ -9,20 +9,9 @@ import UIKit
 
 class PerfilViewController: UIViewController {
     
-    @IBAction func returnButton(_ sender: Any) {
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     
-    @IBAction func tappedReturnButton(_ sender: Any) {
-        navigationController?.popToRootViewController(animated: true)
-    }
-    
-    @IBAction func loggoutButton(_ sender: Any) {
-        navigationController?.dismiss(animated: true)
-        
-    }
 }
