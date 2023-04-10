6064fb36ef59b869dd42f30856ee9f227215bf42
Felipe Augusto <augusto_felipe@icloud.com>
2023-04-10 11:55:54 -0300

    ibactions and iboutlets created

--- TripOrganizer/TripOrganizer/TabBar/Perfil/PerfilViewController.swift
+++ TripOrganizer/TripOrganizer/TabBar/Perfil/PerfilViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class PerfilViewController: UIViewController {
     
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var backButton: UIButton!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var phoneTextField: UITextField!
+    @IBOutlet var changePasswordTextField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var exitButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func exitButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func logoutButtonPressed(_ sender: UIButton) {
+        
+        
+    }
     
 }
