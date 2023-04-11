907bf784171c255489d89d8d77252977263f71a8
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-04-11 16:35:53 -0300

    criando a tela esqueci minha senha, arrumei algumas constraints, arredondei o textField , coloquei as cores e fiz validacoes do textField

--- TripOrganizer/TripOrganizer/RecoverPassworld/RecoverViewController.swift
+++ TripOrganizer/TripOrganizer/RecoverPassworld/RecoverViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class RecoverViewController: UIViewController {
+class RecoverViewController: UIViewController{
+   
+    @IBOutlet weak var recuperarSenhaButton: UIButton!
+    @IBOutlet weak var emailTelaEsquecerSenhaTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        configTextField()
+        configButton()
     }
     
     
     @IBAction func voltarButton(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
+    
+    func configButton(){
+        
+        recuperarSenhaButton.layer.cornerRadius = 10
+        recuperarSenhaButton.clipsToBounds = true
+        
+        
+    }
+    func configTextField(){
+        emailTelaEsquecerSenhaTextField.delegate = self
+        
+        emailTelaEsquecerSenhaTextField.layer.borderWidth = 2
+        emailTelaEsquecerSenhaTextField.layer.borderColor = UIColor(red: 112/255, green: 156/255, blue: 149/255, alpha: 1).cgColor
+        emailTelaEsquecerSenhaTextField.clipsToBounds = true
+        emailTelaEsquecerSenhaTextField.layer.cornerRadius = 10
+        emailTelaEsquecerSenhaTextField.keyboardType = .emailAddress
+        emailTelaEsquecerSenhaTextField.autocorrectionType = .no
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
 }
+
+extension RecoverViewController: UITextFieldDelegate{
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 3
+        textField.layer.borderColor = UIColor(red: 112/255, green: 156/255, blue: 149/255, alpha: 1).cgColor
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor(red: 112/255, green: 156/255, blue: 149/255, alpha: 1).cgColor
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    
+}
+
