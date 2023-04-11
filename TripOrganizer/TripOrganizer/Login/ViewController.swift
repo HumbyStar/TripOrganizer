2289e1349f6850313f85d61353f7bb4745c84edd
Gabriel Mors <gabrielmors210@gmail.com>
2023-04-11 13:59:28 -0400

    feita validacao de cores no textfield na tela de login

--- TripOrganizer/TripOrganizer/Login/ViewController.swift
+++ TripOrganizer/TripOrganizer/Login/ViewController.swift
@@ -25,8 +25,18 @@ class ViewController: UIViewController {
         configButton()
     }
     
+    @IBAction func esqueciSenhatappedButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTappedButton(_ sender: UIButton) {
+//        let vc = UIStoryboard(name: "RegisterController", bundle: nil).instantiateViewController(withIdentifier: "RegisterController") as? RegisterController
+//        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+    }
+    
     @IBAction func acessarButton(_ sender: Any) {
     }
+    
+    
     
     private func configButton() {
         entrarButton.clipsToBounds = true
@@ -40,14 +50,38 @@ class ViewController: UIViewController {
     }
     
     private func configTextField() {
+        emailTextField.delegate = self
+        passWordTextField.delegate = self
+        
+        emailTextField.layer.borderWidth = 2
+        emailTextField.layer.borderColor = UIColor(red: 112/255, green: 156/255, blue: 149/255, alpha: 1).cgColor
         emailTextField.clipsToBounds = true
         emailTextField.layer.cornerRadius = 10
         
+        passWordTextField.layer.borderWidth = 2
+        passWordTextField.layer.borderColor = UIColor(red: 112/255, green: 156/255, blue: 149/255, alpha: 1).cgColor
         passWordTextField.clipsToBounds = true
         passWordTextField.layer.cornerRadius = 10
-
     }
-
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 3
+        textField.layer.borderColor = UIColor(red: 112/255, green: 156/255, blue: 149/255, alpha: 1).cgColor
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor(red: 112/255, green: 156/255, blue: 149/255, alpha: 1).cgColor
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+}
