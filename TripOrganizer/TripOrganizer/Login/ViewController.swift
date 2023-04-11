ad5e112ba6d450f3d366dca126d795f7b591cd5e
Gabriel Mors <gabrielmors210@gmail.com>
2023-04-11 12:42:07 -0400

    refizemos alteracoes na login, mudamos logo e arredondamos os botoes e textfield

--- TripOrganizer/TripOrganizer/Login/ViewController.swift
+++ TripOrganizer/TripOrganizer/Login/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    @IBOutlet weak var entrarButton: UIButton!
+    @IBOutlet weak var entrarGoogleButton: UIButton!
+    @IBOutlet weak var entrarIcloudButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTextField()
+        configButton()
+    }
+    
+    @IBAction func acessarButton(_ sender: Any) {
+    }
+    
+    private func configButton() {
+        entrarButton.clipsToBounds = true
+        entrarButton.layer.cornerRadius = 10
+        
+        entrarGoogleButton.clipsToBounds = true
+        entrarGoogleButton.layer.cornerRadius = 10
+        
+        entrarIcloudButton.clipsToBounds = true
+        entrarIcloudButton.layer.cornerRadius = 10
+    }
+    
+    private func configTextField() {
+        emailTextField.clipsToBounds = true
+        emailTextField.layer.cornerRadius = 10
+        
+        passWordTextField.clipsToBounds = true
+        passWordTextField.layer.cornerRadius = 10
 
+    }
 
+    
 }
 
