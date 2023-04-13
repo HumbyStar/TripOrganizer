da65ed036abdd29b41e561f2e86dab0a635f43e5
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-04-12 22:11:48 -0300

    mudei a largura das celulas da collectionView da tela de pontos turisticos

--- TripOrganizer/TripOrganizer/TabBar/Attraction/AttractionVC.swift
+++ TripOrganizer/TripOrganizer/TabBar/Attraction/AttractionVC.swift
@@ -75,7 +75,7 @@ extension AttractionVC: UICollectionViewDelegate, UICollectionViewDataSource, UI
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let height = collectionView.bounds.height
-        return CGSize(width: 120, height: height - 20)
+        return CGSize(width: 140, height: height - 20)
     }
     
 }
