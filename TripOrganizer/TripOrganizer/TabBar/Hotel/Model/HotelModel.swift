9b6450700462f7a9493398bc904e05a0a84568b0
Mikael <mikaelrodriguespavaneli@gmail.com>
2023-04-18 12:53:59 -0300

    Criei os groups de model e ViewModel e os files dos mesmos

---
+++ TripOrganizer/TripOrganizer/TabBar/Hotel/Model/HotelModel.swift
@@ -0,0 +1,12 @@
+//
+//  HotelModel.swift
+//  TripOrganizer
+//
+//  Created by Mikael Rodrigues on 18/04/23.
+//
+
+import Foundation
+
+struct HotelModel {
+
+}
---
+++ TripOrganizer/TripOrganizer/TabBar/Hotel/ViewModel/HotelViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HotelViewModel.swift
+//  TripOrganizer
+//
+//  Created by Mikael Rodrigues on 18/04/23.
+//
+
+import UIKit
+
+class HotelViewModel {
+
+}
