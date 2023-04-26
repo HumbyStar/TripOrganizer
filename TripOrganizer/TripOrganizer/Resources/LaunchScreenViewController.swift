fe7516d906c21fbd966529c96d2eefd56bee2830
Eduardo Escaleira <duescaleira@gmail.com>
2023-04-26 14:47:15 -0300

    Atualizado com um delay de 1 segundo antes de seguir para a tela de login e codeReview

--- TripOrganizer/TripOrganizer/Resources/LaunchScreenViewController.swift
+++ TripOrganizer/TripOrganizer/Resources/LaunchScreenViewController.swift
@@ -11,38 +11,34 @@ class LaunchScreenViewController: UIViewController {
     
     @IBOutlet weak var logoImageView: UIImageView!
     
-    
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         view.backgroundColor = UIColor.logoCinza
         
         // MARK: ANIMAÇÃO DA IMAGEM COM DURAÇÃO, DELAY E ALTERAÇÃO DA COR DO BACKGROUND:
-//        UIView.animate(withDuration: 2.0, delay: 1.0, animations: { [self] in
-            
-            logoImageView.alpha = 0
-        UIView.animate(withDuration: 1.5, delay: 0, animations: {
+        logoImageView.alpha = 0
+        UIView.animate(withDuration: 2, animations: {
             self.logoImageView.alpha = 1
+        }) { (succes) in
+            
+            // MARK: ADICIONA O DELAY DE 1 SEGUNDO ANTES DE SEGUIR PARA A TELA LOGIN:
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 
-//                            logoImageView.transform = CGAffineTransform(translationX: 0, y: view.bounds.size.height)
-//                            view.backgroundColor = UIColor.logoCinza
-            }) { (succes) in
-                
-                // MARK: SEGUE PARA A TRANSIÇÃO DE TELA, APÓS A ANIMAÇÃO:
-                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-                let vc = storyBoard.instantiateInitialViewController()
-                
-                guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                      let sceneDelegate = windowScene.delegate as? SceneDelegate,
-                      let window = sceneDelegate.window else {
-                    return
-                }
-                
-                //MARK: rootViewController que seguirá na tela (vc/main)
-                window.rootViewController = vc
+            // MARK: SEGUE PARA A TRANSIÇÃO DE TELA, APÓS O DELAY:
+            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyBoard.instantiateInitialViewController()
+            
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                  let sceneDelegate = windowScene.delegate as? SceneDelegate,
+                  let window = sceneDelegate.window else {
+                return
+            }
+            
+            //MARK: rootViewController que seguirá na tela (vc/main)
+            window.rootViewController = vc
             }
         }
-        
     }
+}
 
