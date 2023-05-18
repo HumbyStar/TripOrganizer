2eda051e5137e3a996047067018ec03a00463ef8
Felipe Augusto <augusto_felipe@icloud.com>
2023-05-18 18:43:34 -0300

    lottie

---
+++ TripOrganizer/TripOrganizer/Extension/UIColor  2.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor + Extension.swift
+//  TripOrganizer
+//
+//  Created by Gabriel Mors  on 17/04/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var logoGreen: UIColor {
+        UIColor(red: 112/255, green: 155/255, blue: 149/255, alpha: 1.0)
+    }
+    
+    static var logoOrange: UIColor {
+        UIColor(red: 222/255, green: 120/255, blue: 65/255, alpha: 1.0)
+    }
+    
+    static var logoGray: UIColor {
+        UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+    }
+    
+    static var tabColor: UIColor {
+        UIColor(red: 81/255, green: 157/255, blue: 149/255, alpha: 1.0)
+    }
+}
